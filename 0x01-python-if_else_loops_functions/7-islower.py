4b25d5e57ce9fda5deea8b3e57a5c21329600ca7
aisosa <lahoginger99@gmail.com>
2023-03-07 12:54:03 -0800

    7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def islower(c):
-    if ord(c) >= 97 and ord(c) < 123:
-        return(True)
+    """Function checks for lowercase characters."""
+        if ord(c) >= 97 and ord(c) <= 122:
+            return True
 else:
-    return(False)
+    return False
